bc8f4160fce35df78cee9d65e5f48498d66d3e11
Nguyen Dinh Phu <phu0981838248@gmail.com>
2025-08-26 17:08:37 +0700

    feat(Room): impl roomService

--- src/main/java/com/ticketmanagement/ticketing/services/impl/RoomServiceImpl.java
+++ src/main/java/com/ticketmanagement/ticketing/services/impl/RoomServiceImpl.java
@@ -1,33 +1,54 @@
 package com.ticketmanagement.ticketing.services.impl;
 
 import com.ticketmanagement.ticketing.dto.RoomDTO;
+import com.ticketmanagement.ticketing.mapper.RoomMapper;
+import com.ticketmanagement.ticketing.model.entity.RoomEntity;
+import com.ticketmanagement.ticketing.repository.RoomRepository;
 import com.ticketmanagement.ticketing.services.RoomService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
 public class RoomServiceImpl implements RoomService {
+
+    @Autowired
+    private RoomRepository roomRepository;
+
     @Override
     public RoomDTO createRoom(RoomDTO roomDTO) {
-        return null;
+        RoomEntity room = RoomMapper.toEntity(roomDTO);
+        roomRepository.save(room);
+        return RoomMapper.toDTO(room);
     }
 
     @Override
     public RoomDTO getRoomById(Integer roomId) {
-        return null;
+        RoomEntity room = roomRepository.findById(roomId)
+                .orElseThrow(() -> new RuntimeException("Room not found"));
+        return RoomMapper.toDTO(room);
     }
 
     @Override
     public List<RoomDTO> getAllRooms() {
-        return List.of();
+        return roomRepository.findAll()
+                .stream().map(RoomMapper::toDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
     public RoomDTO updateRoom(Integer roomId, RoomDTO roomDTO) {
-        return null;
+        RoomEntity room = roomRepository.findById(roomId)
+                .orElseThrow(() -> new RuntimeException("Room not found"));
+        room.setRoomName(roomDTO.getRoomName());
+        roomRepository.save(room);
+        return RoomMapper.toDTO(room);
     }
 
     @Override
     public void deleteRoom(Integer roomId) {
-
+        roomRepository.deleteById(roomId);
     }
 }
