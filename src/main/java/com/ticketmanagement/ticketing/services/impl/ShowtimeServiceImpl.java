ef1d93c91c3de7fdddbf7993ed0baff526c67169
Nguyen Dinh Phu <phu0981838248@gmail.com>
2025-08-26 17:07:25 +0700

    feat(Showtime): impl showtimeService

--- src/main/java/com/ticketmanagement/ticketing/services/impl/ShowtimeServiceImpl.java
+++ src/main/java/com/ticketmanagement/ticketing/services/impl/ShowtimeServiceImpl.java
@@ -1,33 +1,55 @@
 package com.ticketmanagement.ticketing.services.impl;
 
 import com.ticketmanagement.ticketing.dto.ShowtimeDTO;
+import com.ticketmanagement.ticketing.mapper.ShowtimeMapper;
+import com.ticketmanagement.ticketing.model.entity.ShowtimeEntity;
+import com.ticketmanagement.ticketing.repository.ShowtimeRepository;
 import com.ticketmanagement.ticketing.services.ShowtimeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
 public class ShowtimeServiceImpl implements ShowtimeService {
+
+    @Autowired
+    private ShowtimeRepository showtimeRepository;
+
     @Override
     public ShowtimeDTO createShowtime(ShowtimeDTO showtimeDTO) {
-        return null;
+        ShowtimeEntity showtime = ShowtimeMapper.toEntity(showtimeDTO);
+        showtimeRepository.save(showtime);
+        return ShowtimeMapper.toDTO(showtime);
     }
 
     @Override
     public ShowtimeDTO getShowtimeById(Integer showtimeId) {
-        return null;
+        ShowtimeEntity showtime = showtimeRepository.findById(showtimeId)
+                .orElseThrow(() -> new RuntimeException("Showtime not found"));
+        return ShowtimeMapper.toDTO(showtime);
     }
 
     @Override
     public List<ShowtimeDTO> getAllShowtimes() {
-        return List.of();
+        return showtimeRepository.findAll()
+                .stream().map(ShowtimeMapper::toDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
     public ShowtimeDTO updateShowtime(Integer showtimeId, ShowtimeDTO showtimeDTO) {
-        return null;
+        ShowtimeEntity showtime = showtimeRepository.findById(showtimeId)
+                .orElseThrow(() -> new RuntimeException("Showtime not found"));
+        showtime.setStartTime(showtimeDTO.getStartTime());
+        showtime.setEndTime(showtimeDTO.getEndTime());
+        showtimeRepository.save(showtime);
+        return ShowtimeMapper.toDTO(showtime);
     }
 
     @Override
     public void deleteShowtime(Integer showtimeId) {
-
+        showtimeRepository.deleteById(showtimeId);
     }
 }
