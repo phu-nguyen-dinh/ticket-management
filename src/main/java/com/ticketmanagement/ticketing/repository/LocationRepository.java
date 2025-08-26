b39af49f01feda5de8f4847e158cd0b77f986680
Nguyen Dinh Phu <phu0981838248@gmail.com>
2025-08-26 17:10:41 +0700

    feat(Location): impl locationService

--- src/main/java/com/ticketmanagement/ticketing/repository/LocationRepository.java
+++ src/main/java/com/ticketmanagement/ticketing/repository/LocationRepository.java
@@ -1,6 +1,7 @@
 package com.ticketmanagement.ticketing.repository;
 
+import com.ticketmanagement.ticketing.model.entity.LocationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LocationRepository extends JpaRepository<LocationRepository, Integer> {
+public interface LocationRepository extends JpaRepository<LocationEntity, Integer> {
 }
--- src/main/java/com/ticketmanagement/ticketing/services/impl/LocationServiceImpl.java
+++ src/main/java/com/ticketmanagement/ticketing/services/impl/LocationServiceImpl.java
@@ -1,33 +1,56 @@
 package com.ticketmanagement.ticketing.services.impl;
 
 import com.ticketmanagement.ticketing.dto.LocationDTO;
+import com.ticketmanagement.ticketing.mapper.LocationMapper;
+import com.ticketmanagement.ticketing.model.entity.LocationEntity;
+import com.ticketmanagement.ticketing.repository.LocationRepository;
 import com.ticketmanagement.ticketing.services.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
 public class LocationServiceImpl implements LocationService {
+
+    @Autowired
+    private LocationRepository locationRepository;
+
     @Override
     public LocationDTO createLocation(LocationDTO locationDTO) {
-        return null;
+        LocationEntity location = LocationMapper.toEntity(locationDTO);
+        locationRepository.save(location);
+        return LocationMapper.toDTO(location);
     }
 
     @Override
     public LocationDTO getLocationById(Integer locationId) {
-        return null;
+        LocationEntity location = locationRepository.findById(locationId)
+                .orElseThrow(() -> new RuntimeException("Location not found"));
+        return LocationMapper.toDTO(location);
     }
 
     @Override
     public List<LocationDTO> getAllLocations() {
-        return List.of();
+        return locationRepository.findAll()
+                .stream().map(LocationMapper::toDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
     public LocationDTO updateLocation(Integer locationId, LocationDTO locationDTO) {
-        return null;
+        LocationEntity location = locationRepository.findById(locationId)
+                .orElseThrow(() -> new RuntimeException("Location not found"));
+        location.setLocationName(locationDTO.getLocationName());
+        location.setAddress(locationDTO.getAddress());
+        location.setCapacity(locationDTO.getCapacity());
+        locationRepository.save(location);
+        return LocationMapper.toDTO(location);
     }
 
     @Override
     public void deleteLocation(Integer locationId) {
-
+        locationRepository.deleteById(locationId);
     }
 }
