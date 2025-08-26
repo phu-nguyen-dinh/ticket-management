daab978f5b967484b4920c76c6368a0c88710dd8
Nguyen Dinh Phu <phu0981838248@gmail.com>
2025-08-26 17:00:31 +0700

    feat(ticket): impl ticketService

--- src/main/java/com/ticketmanagement/ticketing/services/impl/TicketServiceImpl.java
+++ src/main/java/com/ticketmanagement/ticketing/services/impl/TicketServiceImpl.java
@@ -1,33 +1,51 @@
 package com.ticketmanagement.ticketing.services.impl;
 
 import com.ticketmanagement.ticketing.dto.TicketDTO;
+import com.ticketmanagement.ticketing.mapper.TicketMapper;
+import com.ticketmanagement.ticketing.model.entity.TicketEntity;
+import com.ticketmanagement.ticketing.repository.TicketRepository;
 import com.ticketmanagement.ticketing.services.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TicketServiceImpl implements TicketService {
+    @Autowired
+    private TicketRepository ticketRepository;
+
     @Override
     public TicketDTO createTicket(TicketDTO ticketDTO) {
-        return null;
+        TicketEntity ticket = TicketMapper.toEntity(ticketDTO);
+        ticketRepository.save(ticket);
+        return TicketMapper.toDTO(ticket);
     }
 
     @Override
     public TicketDTO getTicketById(Integer ticketId) {
-        return null;
+        TicketEntity ticket = ticketRepository.findById(ticketId)
+                .orElseThrow(() -> new RuntimeException("Ticket not found"));
+        return TicketMapper.toDTO(ticket);
     }
 
     @Override
     public List<TicketDTO> getAllTickets() {
-        return List.of();
+        return ticketRepository.findAll()
+                .stream().map(TicketMapper::toDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
     public TicketDTO updateTicket(Integer ticketId, TicketDTO ticketDTO) {
-        return null;
+        TicketEntity ticket = ticketRepository.findById(ticketId)
+                .orElseThrow(() -> new RuntimeException("Ticket not found"));
+        ticket.setPrice(ticketDTO.getPrice());
+        ticketRepository.save(ticket);
+        return TicketMapper.toDTO(ticket);
     }
 
     @Override
     public void deleteTicket(Integer ticketId) {
-
+        ticketRepository.deleteById(ticketId);
     }
 }
