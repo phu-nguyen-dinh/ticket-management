308391646cd4089b34e408951e202aadac0d8e99
Nguyen Dinh Phu <phu0981838248@gmail.com>
2025-08-26 17:04:39 +0700

    feat(User): impl userService

--- src/main/java/com/ticketmanagement/ticketing/services/impl/UserServiceImpl.java
+++ src/main/java/com/ticketmanagement/ticketing/services/impl/UserServiceImpl.java
@@ -1,33 +1,55 @@
 package com.ticketmanagement.ticketing.services.impl;
 
 import com.ticketmanagement.ticketing.dto.UserDTO;
+import com.ticketmanagement.ticketing.mapper.UserMapper;
+import com.ticketmanagement.ticketing.model.entity.UserEntity;
+import com.ticketmanagement.ticketing.repository.UserRepository;
 import com.ticketmanagement.ticketing.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public UserDTO createUser(UserDTO userDTO) {
-        return null;
+        UserEntity user = UserMapper.toEntity(userDTO);
+        userRepository.save(user);
+        return UserMapper.toDTO(user);
     }
 
     @Override
     public UserDTO getUserById(Integer userId) {
-        return null;
+        UserEntity user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        return UserMapper.toDTO(user);
     }
 
     @Override
     public List<UserDTO> getAllUsers() {
-        return List.of();
+        return userRepository.findAll()
+                .stream().map(UserMapper::toDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
     public UserDTO updateUser(Integer userId, UserDTO userDTO) {
-        return null;
+        UserEntity user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        user.setUsername(userDTO.getUsername());
+        user.setPassword(userDTO.getPassword());
+        userRepository.save(user);
+        return UserMapper.toDTO(user);
     }
 
     @Override
     public void deleteUser(Integer userId) {
-
+        userRepository.deleteById(userId);
     }
 }
